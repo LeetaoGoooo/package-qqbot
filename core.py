2cb1349967b10729a0500ff2baa59838c39e1ca6
lt94 <leetao94cn@gmail.com>
2018-03-05 19:11:25 +0800

    add local message contact(fix #1)

--- core.py
+++ core.py
@@ -6,10 +6,14 @@ user_package = Package()
 
 @qqbotslot
 def onQQMessage(bot,contact,member,content):
-    # print(contact.name)
     if not bot.isMe(contact,member):
+        # 群组聊天
         if member is not None:
             text = user_package.handle_message_str(member.name,content)
             bot.SendTo(contact,member.name+" :"+text)
+        # 一对一聊天
+        else:
+            text = user_package.handle_message_str(contact,content)
+            bot.SendTo(contact,text)
 
 RunBot()
